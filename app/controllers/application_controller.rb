3aabf1a7924490e2e2a1bddaf1bce653358cdad4
toku-kou8 <tokunaga.kouichi.infratop@gmail.com>
2022-08-17 15:08:48 +0000

    devise first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_up_path_for(resource)
+    case resource
+    when Admin
+      root_path
+    when Customer
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin   # ログアウト時はシンボルが返ってくる
+      root_path
+    when :customer  # ログアウト時はシンボルが返ってくる
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :customer, controllers: {
+    sessions: 'public/sessions',
+    passwords: 'public/passwords',
+    registrations: 'public/registrations'
+  }
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+  root to: 'public/homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
